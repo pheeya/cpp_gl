7d23ff576ae82b55043722832befba29ecaad814
pheeyam <sameerahmed1472@gmail.com>
2023-08-28 03:12:08 +0500

    added imgui

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,28 @@
 #include <GLFW/glfw3.h>
 #include <spdlog/spdlog.h>
 #include <iostream>
-
+#include <imgui.h>
+#include <backends/imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
 const GLuint WIDTH = 800, HEIGHT = 600;
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+void InitImgui(GLFWwindow *_window)
+{
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO &io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // IF using Docking Branch
+    ImGui_ImplGlfw_InitForOpenGL(_window, true);
+    ImGui_ImplOpenGL3_Init();
 }
 
 int main(void)
@@ -24,6 +39,7 @@ int main(void)
 
     glfwSetKeyCallback(window, key_callback);
 
+    InitImgui(window);
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         spdlog::critical("failed to initialize gl context");
@@ -35,8 +51,16 @@ int main(void)
     {
         glfwPollEvents();
 
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        ImGui::ShowDemoWindow();
+
         glClearColor(0.4f, 0.7f, 0.4f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
     }
